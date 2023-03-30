1168bf3dd09386215c62a8f25e3fdf77e41cf08c
EverittGill <eg.everittgill@gmail.com>
2023-03-30 18:33:01 -0400

    added a functional test for the triangle test and a not very functional test for the square

---
+++ tests/square.test.js
@@ -0,0 +1,9 @@
+const Triangle = require('../lib/square');
+
+describe('Square', () => {
+    it('needs to return the right SVG data', () => {
+        const square = new Square('blue');
+        const expected = '<rect x="10" y="10" width="300" height="300" fill="blue"/>'
+        expect(square.render()).toEqual(expected);
+    });
+});
--- tests/triangle.test.js
+++ tests/triangle.test.js
@@ -3,3 +3,16 @@
 // const shape = new Triangle();
 // shape.setColor("blue");
 // expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+
+
+
+const Triangle = require('../lib/triangle');
+
+describe('Triangle', () => {
+    it('needs to return the right SVG data', () => {
+        const triangle = new Triangle('blue');
+        const expected = '<polygon points="150,50 300,200 0,200" fill="blue" />'
+        expect(triangle.render()).toEqual(expected);
+    });
+});
+
